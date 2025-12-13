3ae430b23888b4bbca30308818da44492c140b91
Hayashi <33160326+HayashiNiizuki@users.noreply.github.com>
2025-12-13 23:16:25 +0800

    Add custom tab title formatting based on process

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -31,5 +31,41 @@ wezterm.on('gui-startup', function(window)
   gui_window:perform_action(wezterm.action.ToggleFullScreen, pane)
 end)
 
+wezterm.on('format-tab-title', function(tab, tabs, panes, config, hover, max_width)
+    local pane = tab.active_pane
+    local title = tab.tab_title
+
+    if not title or #title == 0 then
+        local process_name = pane.foreground_process_name
+
+        if process_name then
+            process_name = process_name:match("([^/\\]+)$") or process_name
+
+            local is_shell = process_name:match("bash") or process_name:match("zsh") or process_name:match("fish") or
+                                 process_name:match("sh$")
+
+            if is_shell then
+                local cwd_uri = pane.current_working_dir
+                if cwd_uri then
+                    local cwd = cwd_uri.file_path or tostring(cwd_uri)
+                    title = cwd:match("([^/]+)/?$") or cwd
+                else
+                    title = process_name
+                end
+            else
+                title = process_name
+            end
+        else
+            title = "Tab"
+        end
+    end
+
+    local index = tab.tab_index + 1
+
+    return {{
+        Text = ' ' .. index .. ': ' .. title .. ' '
+    }}
+end)
+
 -- Finally, return the configuration to wezterm:
 return config
